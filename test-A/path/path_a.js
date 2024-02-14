29cf96a68752caaba1eac2beffd4a5151e7c8a10
Mauricio Cox <maucox77@gmail.com>
2024-02-14 15:52:07 -0300

    ejemplo ls

--- test-A/path/path_a.js
+++ test-A/path/path_a.js
@@ -7,14 +7,14 @@ const filePath = path.join('content', 'sub-folder', 'test.txt');
 console.log('Path Join: ',filePath); // devuleve la union de las rutas: \content\sub-folder\test.txt
 
 const baseName = path.basename(filePath);
-console.log('Base Name: ',baseName);
+console.log('Base Name: ',baseName); // test.txt
 
 const url = path.basename('/user/profile/user-data/passwod.txt');
-console.log('Base Name: ',url)
+console.log('Base Name: ',url); //passwod.txt
 
 // Podemos quitar tambien la extension del documento agregando un parametro
 const noExtension = path.basename('/mi-site/products/my-cart.html', '.html');
-console.log('Base Name no extension: ',noExtension);
+console.log('Base Name no extension: ',noExtension); // my-cart
 
 const extension = path.extname('hero-bg.webp');
-console.log('Extension name: ', extension);
+console.log('Extension name: ', extension); // .webp
