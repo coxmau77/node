43838055239857dda877b4b77059f27180a440c3
Mauricio Cox <coxmau77@gmail.com>
2024-02-20 17:21:33 -0300

    Ejemplo servidor con puerto dinamico para desarrollo

--- test-A/http/index.js
+++ test-A/http/index.js
@@ -1,2 +1,19 @@
+// Servidor web node desde cero nativo
+const http = require('node:http');
 
-console.log("Hola mundo http!");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((request, response) => {
+
+    console.log("Mensaje por consola en backend!")
+
+    response.end('El server recibio la peticion (request)');
+});
+
+// server.listen(port, () => {
+//     console.log(`Server on http://localhost:${port}`);
+// });
+
+server.listen(0, () => {
+    console.log(`Server on http://localhost:${server.address().port}`);
+});
