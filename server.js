d0fa636f09d128e62c52c607d622ffcd715113ff
Mauricio Cox <coxmau77@gmail.com>
2024-08-01 17:00:57 -0300

    chore:Instalación de Express.js y tu primer servidor HTTP

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Ejecutando server.js ...");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Definir una ruta
+app.get('/', (request, response) =>{
+  response.send('Ruta principal del servidor');
+});
+
+// Iniciar servidor
+app.listen(PORT, () =>{
+  console.log(`Servidor corriendo en >>> http://localhost:${PORT}`);
+});
