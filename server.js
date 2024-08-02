e936ad5974b5ec277ab06dec0952102d9df8d766
Mauricio Cox <coxmau77@gmail.com>
2024-08-01 21:49:00 -0300

    chore: Routing con Express.js

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const app = express();
 const PORT = 3000;
 
 // Definir una ruta
-app.get('/', (request, response) =>{
+app.get('/', (request, response) => {
   response.send('Ruta principal del servidor');
 });
-app.get('/new', (request, response) =>{
+app.get('/new', (request, response) => {
   response.send('Nueva ruto o endpoint');
+});
+app.get('/products', (request, response) => {
+  response.json({
+    title: 'Título del producto',
+    description: 'Breve descripción del producto',
+    sku: 'ARG2024',
+    price: 2499.99,
+    stock: 48,
+    onsale: true,
+    date: '2024-08-01',
+    category: 'Categoría_3',
+  });
 });
 
 // Iniciar servidor
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en >>> http://localhost:${PORT}`);
 });
