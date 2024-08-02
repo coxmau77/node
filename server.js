18e00684c167136f691e2399d8225809b4c17dff
Mauricio Cox <coxmau77@gmail.com>
2024-08-02 00:32:59 -0300

    chore: GET: recibir parámetros

--- server.js
+++ server.js
@@ -6,19 +6,51 @@ const PORT = 3000;
 app.get('/', (request, response) => {
   response.send('Ruta principal del servidor');
 });
+
 app.get('/new', (request, response) => {
   response.send('Nueva ruto o endpoint');
 });
+
 app.get('/products', (request, response) => {
+  response.json([
+    {
+      title: 'Título del producto #1',
+      description: 'Breve descripción del producto',
+      sku: 'ARG2024',
+      price: 2499.99,
+      stock: 48,
+      onsale: false,
+      date: '2024-08-01',
+      category: 'Categoría_3',
+    },
+    {
+      title: 'Título del producto #2',
+      description: 'Breve descripción del producto',
+      sku: 'BSAS2024',
+      price: 3499.99,
+      stock: 64,
+      onsale: true,
+      date: '2024-08-01',
+      category: 'Categoría_5',
+    },
+  ]);
+});
+
+app.get('/products/:id', (request, response) => {
+  const { id } = request.params;
+
   response.json({
-    title: 'Título del producto',
-    description: 'Breve descripción del producto',
-    sku: 'ARG2024',
-    price: 2499.99,
-    stock: 48,
-    onsale: true,
-    date: '2024-08-01',
-    category: 'Categoría_3',
+    id,
+    description: 'Devuelve un producto por ID',
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (request, response) => {
+  const { categoryId, productId } = request.params;
+  response.json({
+    categoryId,
+    productId,
+    description: 'Devuelve un producto por varios parametros',
   });
 });
 
